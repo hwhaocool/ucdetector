f18ca467bcecd48a5cdf5ae112a771da80d1140f
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2008-09-09 21:27:27 +0000

    refactor report parameter

--- org.ucdetector/src/org/ucdetector/report/IUCDetctorReport.java
+++ org.ucdetector/src/org/ucdetector/report/IUCDetctorReport.java
@@ -7,32 +7,11 @@
 package org.ucdetector.report;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.jdt.core.IJavaElement;
-import org.ucdetector.preferences.WarnLevel;
 
 /**
  *
  */
 public interface IUCDetctorReport {
-  public static class ReportParam {
-    public IJavaElement javaElement;
-    public String message;
-    public int line;
-    public WarnLevel level;
-    public String markerType;
-    public String problem;
-
-    public ReportParam(IJavaElement javaElement, String message, int line,
-        WarnLevel level, String markerType, String problem) {
-      this.javaElement = javaElement;
-      this.message = message;
-      this.line = line;
-      this.level = level;
-      this.markerType = markerType;
-      this.problem = problem;
-    }
-  }
-
   void reportMarker(ReportParam reportParam) throws CoreException;
 
   void endReport(Object[] selected, long start) throws CoreException;
