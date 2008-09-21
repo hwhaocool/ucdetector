11cbcb984daeb7e2ace56ce3aa58814695970de3
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2008-09-21 20:43:05 +0000

    better QuickFix

--- org.ucdetector/src/org/ucdetector/util/UCDQuickFixer.java
+++ org.ucdetector/src/org/ucdetector/util/UCDQuickFixer.java
@@ -10,16 +10,21 @@ package org.ucdetector.util;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.ui.IMarkerResolution;
-import org.eclipse.ui.IMarkerResolutionGenerator;
+import org.eclipse.ui.IMarkerResolutionGenerator2;
+import org.ucdetector.Log;
+import org.ucdetector.UCDetectorPlugin;
 
 /**
  * Generates Marker Resolutions<br>
  * @see http://wiki.eclipse.org/FAQ_How_do_I_implement_Quick_Fixes_for_my_own_language%3F
  * @see extension point="org.eclipse.ui.ide.markerResolution" in plugin.xml
  */
-public class UCDQuickFixer implements IMarkerResolutionGenerator { // NO_UCD
+public class UCDQuickFixer implements IMarkerResolutionGenerator2 { // NO_UCD
   public IMarkerResolution[] getResolutions(IMarker marker) {
     try {
+      if (UCDetectorPlugin.DEBUG) {
+        Log.logDebug("UCDQuickFixer.getResolutions()" + marker); //$NON-NLS-1$
+      }
       // we can use only use String, Boolean, Integer here,
       // no IJavaElements are permitted here!
       String javaElement = (String) marker
@@ -29,8 +34,13 @@ public class UCDQuickFixer implements IMarkerResolutionGenerator { // NO_UCD
       };
     }
     catch (CoreException e) {
+      Log.logError("Can't get UCD resolutions", e);
       e.printStackTrace();
     }
     return new IMarkerResolution[0];
+  }
+
+  public boolean hasResolutions(IMarker marker) {
+    return true;
   }
 }
