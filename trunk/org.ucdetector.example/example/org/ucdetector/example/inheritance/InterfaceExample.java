278307a1c910afbba34d82777a952d10ebea49e3
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2008-11-13 21:39:24 +0000

    Fix Bug [ 2269486 ] Constants in Interfaces Can't be Private

--- trunk/org.ucdetector.example/example/org/ucdetector/example/inheritance/InterfaceExample.java
+++ trunk/org.ucdetector.example/example/org/ucdetector/example/inheritance/InterfaceExample.java
@@ -5,6 +5,20 @@ public interface InterfaceExample {
   // UNUSED
   // -------------------------------------------------------------------------
   public static final String UNUSED = "UNUSED"; // Marker YES: unused code
+  /**
+   * Due to the Java Language Spec members in interfaces can't be private.
+   *   Therefore UCDetector should not propose to set constants to private, when
+   *   these constants are only used within the same interface (e.g. when used to
+   *   calculate other constant values).
+   *   ACTUAL BEHAVIOR
+   * 
+   *   UCDetector proposes to make constants in interfaces private. This will
+   *   produce invalid Java source.
+   * @see https://sourceforge.net/tracker/index.php?func=detail&aid=2269486&group_id=219599&atid=1046865
+   */
+  public static final String BUG_2269486 = "USED";
+
+  public static final String UNUSED_2 = "UNUSED_2" + BUG_2269486; // Marker YES: unused code
 
   // -------------------------------------------------------------------------
   // USED
