e68ba8c9e9692465d517f39f12c874544163a451
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2008-11-13 21:39:24 +0000

    Fix Bug [ 2269486 ] Constants in Interfaces Can't be Private

---
+++ org.ucdetector.example/example/org/ucdetector/example/inheritance/UnusedInterfaceExample.java
@@ -0,0 +1,7 @@
+package org.ucdetector.example.inheritance;
+
+public interface UnusedInterfaceExample {
+
+  public static final String CLASS_NAME = UnusedInterfaceExample.class
+      .getName();
+}
