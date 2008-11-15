b899fbb3ae21685bef93b2f5bbdb76ffbd7c8a2d
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2008-11-15 21:35:45 +0000

    interfaces cant be private

--- org.ucdetector.example/example/org/ucdetector/example/inheritance/UnusedInterfaceExample.java
+++ org.ucdetector.example/example/org/ucdetector/example/inheritance/UnusedInterfaceExample.java
@@ -1,9 +1,7 @@
 package org.ucdetector.example.inheritance;
 // interfaces can't be private
-public interface UnusedInterfaceExample { // Marker YES: unused code
+public interface UnusedInterfaceExample {
 
-	
   // interface methods can't be private
-  public static final String CLASS_NAME = UnusedInterfaceExample.class
-      .getName();
+  public static final String CLASS_NAME = UnusedInterfaceExample.class.getName();  // Marker YES: unused code
 }
