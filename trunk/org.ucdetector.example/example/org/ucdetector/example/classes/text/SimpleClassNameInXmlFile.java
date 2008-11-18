93ef9544da088161e9f148695001056861b1688e
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2008-11-18 21:44:56 +0000

    Search for classname

---
+++ trunk/org.ucdetector.example/example/org/ucdetector/example/classes/text/SimpleClassNameInXmlFile.java
@@ -0,0 +1,10 @@
+package org.ucdetector.example.classes.text;
+
+/**
+ * Simple class name exists in
+ * /org.ucdetector_test/example/org/ucdetector/example/no_java/reference.xml
+ * Detection depends on "File name patterns"
+ */
+public class SimpleClassNameInXmlFile {
+
+}
