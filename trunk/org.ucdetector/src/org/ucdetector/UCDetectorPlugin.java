c7e55a3884bd8f8b96c600e66374a417be156abd
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-01-27 21:46:37 +0000

    +ignoreTestOnlyFilter

--- trunk/org.ucdetector/src/org/ucdetector/UCDetectorPlugin.java
+++ trunk/org.ucdetector/src/org/ucdetector/UCDetectorPlugin.java
@@ -66,19 +66,19 @@ public class UCDetectorPlugin extends AbstractUIPlugin {
     sb.append("\r\njava=").append(System.getProperty("java.version")); //$NON-NLS-1$ //$NON-NLS-2$
     sb.append(",eclipse=").append(System.getProperty("osgi.framework.version")); //$NON-NLS-1$ //$NON-NLS-2$
     sb.append("\r\nlogfile=").append(System.getProperty("osgi.logfile")); //$NON-NLS-1$ //$NON-NLS-2$
-    sb.append("\r\n---- Plugin Preferences -------------------"); //$NON-NLS-1$
     sb.append(getPreferencesAsString());
-    sb.append("\r\n-----------------------------------------------"); //$NON-NLS-1$
     Log.logInfo(sb.toString());
   }
 
-  private static String getPreferencesAsString() {
+  public static String getPreferencesAsString() {
     StringBuilder sb = new StringBuilder();
+    sb.append("\r\n---- UCDetector Plugin Preferences -----------"); //$NON-NLS-1$
     String[] propertyNames = plugin.getPluginPreferences().propertyNames();
     for (String propertyName : propertyNames) {
       sb.append("\r\n   ").append(propertyName).append("="); //$NON-NLS-1$ //$NON-NLS-2$
       sb.append(plugin.getPluginPreferences().getString(propertyName));
     }
+    sb.append("\r\n-----------------------------------------------"); //$NON-NLS-1$
     return sb.toString();
   }
 
