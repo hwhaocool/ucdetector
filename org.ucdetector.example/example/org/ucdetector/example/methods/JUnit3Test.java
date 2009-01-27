ae2df0d6aeb6c6ada984141fffe03167862c14fc
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-01-27 23:03:20 +0000

    +examples for isJUnitTestMethod()

---
+++ org.ucdetector.example/example/org/ucdetector/example/methods/JUnit3Test.java
@@ -0,0 +1,10 @@
+package org.ucdetector.example.methods;
+/**
+ *
+ */
+public class JUnit3Test /* extends org.junit.TestCase*/{
+  public void testExample() {
+    JUnitExample example = new JUnitExample();
+    //    example.booleanMethod();
+  }
+}
