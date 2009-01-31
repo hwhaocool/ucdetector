13ce49fe5f33d9411872ad723dbac14ad6d33976
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-01-31 19:39:08 +0000

    add test only examples

---
+++ trunk/org.ucdetector.example/reference/org/ucdetector/example/impl/JUnitReferenceHolder.java
@@ -0,0 +1,31 @@
+package org.ucdetector.example.impl;
+
+import org.ucdetector.example.ReferencedByTestsExample;
+
+public class JUnitReferenceHolder {
+
+  /**
+   * This IS a JUnit test method
+   */
+  public void testHoldReference() {
+    ReferencedByTestsExample example = new ReferencedByTestsExample();
+    example.referencedByTestMethod();
+  }
+
+  /**
+   * This is NOT a JUnit test method
+   */
+  public static void testHoldReferenceStatic() {
+    ReferencedByTestsExample example = new ReferencedByTestsExample();
+    example.referencedByWrongTestMethodStatic();
+  }
+
+  /**
+   * This is NOT a JUnit test method
+   */
+  public void testHoldReference(int i) {
+    ReferencedByTestsExample example = new ReferencedByTestsExample();
+    example.referencedByWrongTestMethod();
+  }
+
+}
---
+++ trunk/org.ucdetector.example/src/test/org/ucdetector/example/ReferenceHolderInTestSourceFolder.java
@@ -0,0 +1,8 @@
+package org.ucdetector.example;
+
+public class ReferenceHolderInTestSourceFolder {
+  public static void main(String[] args) {
+    ReferencedByTestsExample example = new ReferencedByTestsExample();
+    example.referencedByTestSourceFolder();
+  }
+}
