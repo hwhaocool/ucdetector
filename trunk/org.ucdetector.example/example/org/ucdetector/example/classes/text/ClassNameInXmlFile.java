3380c56bc8aaf82c7fcc554febf6738c844cb792
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-02-01 00:34:47 +0000

    fix [ 2373808 ] Classes found by text search should have no markers

--- trunk/org.ucdetector.example/example/org/ucdetector/example/classes/text/ClassNameInXmlFile.java
+++ trunk/org.ucdetector.example/example/org/ucdetector/example/classes/text/ClassNameInXmlFile.java
@@ -7,4 +7,21 @@ package org.ucdetector.example.classes.text;
  */
 public class ClassNameInXmlFile {
 
+	/**
+	 * There should be no marker here!
+	 * <p>
+	 * see also [ 2373808 ] Classes found by text search should have no markers:<br>
+	 * https://sourceforge.net/tracker2/?func=detail&aid=2373808&group_id=219599&atid=1046865
+	 */
+	public static final int UNUSED = -1;
+
+	/**
+	 * There should be no marker here! see also [ 2373808 ] Classes found by
+	 * <p>
+	 * text search should have no markers:<br>
+	 * https://sourceforge.net/tracker2/?func=detail&aid=2373808&group_id=219599&atid=1046865
+	 */
+	public void unused() {
+
+	}
 }
