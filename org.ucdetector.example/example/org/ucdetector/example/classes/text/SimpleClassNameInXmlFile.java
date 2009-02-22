2b2f654ab835cbf9808a13fb7c30f813c81fa819
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-02-22 11:16:30 +0000

    test

--- org.ucdetector.example/example/org/ucdetector/example/classes/text/SimpleClassNameInXmlFile.java
+++ org.ucdetector.example/example/org/ucdetector/example/classes/text/SimpleClassNameInXmlFile.java
@@ -3,13 +3,11 @@ package org.ucdetector.example.classes.text;
 /**
  * Simple class name exists in
  * /org.ucdetector_test/example/org/ucdetector/example/no_java/reference.xml
- * 
- * <p>
- * 
- * Detection depends on "File name patterns"
+ * <b>NOTE: Detection depends on "File name patterns"</b> <br>
+ * <b>NOTE: Detection depends on "Search full class name"</b>
  */
 public class SimpleClassNameInXmlFile { // Marker YES: unused code
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	// to test this class, change settings "File name patterns"
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  // to test this class, change settings "File name patterns"
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
