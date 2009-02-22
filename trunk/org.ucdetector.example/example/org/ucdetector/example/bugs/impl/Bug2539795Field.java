1f20619d2665a8a63008a683a230ad6038ab5a4f
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-02-22 11:00:33 +0000

    test

--- trunk/org.ucdetector.example/example/org/ucdetector/example/bugs/impl/Bug2539795Field.java
+++ trunk/org.ucdetector.example/example/org/ucdetector/example/bugs/impl/Bug2539795Field.java
@@ -1,10 +1,12 @@
 package org.ucdetector.example.bugs.impl;
+
 /**
- * Bug: Bug2539795Field may not be default, because Bug2539795Field.getBar()
- * is used outside of this package! 
+ * Bug: Bug2539795Field may not be default, because Bug2539795Field.getBar() is
+ * used outside of this package!
  */
+// TODO: Fix bug (no marker here, because public method is used)
 public class Bug2539795Field {
-  public String getBar() {
-    return "Bar";
-  }
+	public String getBar() {
+		return "Bar";
+	}
 }
