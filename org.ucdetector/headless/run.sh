a32fda4e6338c0b3363f3e276de86b63563a0f9b
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-03-01 01:38:00 +0000

    move headless stuff

--- org.ucdetector/headless/run.sh
+++ org.ucdetector/headless/run.sh
@@ -4,7 +4,7 @@
 export launcher=/opt/eclipse-3.4/plugins/org.eclipse.equinox.launcher_1.0.100.v20080509-1800.jar
 #export launcher=/opt/eclipse-3.4M5/plugins/org.eclipse.equinox.launcher_1.0.100.v20071211.jar
 
-export workspace=/home/js/workspace/runtime-EclipseApplication
+export workspace=F:/ws/ucd\/runtime-ucd_configuration
 
-java -jar $launcher -application -consolelog org.eclipse.ant.core.antRunner -data $workspace -file headless.xml -debug
+java -jar $launcher -application org.eclipse.ant.core.antRunner -data $workspace -file headless.xml -consolelog -debug
 
