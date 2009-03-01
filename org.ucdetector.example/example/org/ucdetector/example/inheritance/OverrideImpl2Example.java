c1e97aee1ba45a7c3e3f26d19c46408ea8c936d8
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-03-01 22:06:27 +0000

    more examples

---
+++ org.ucdetector.example/example/org/ucdetector/example/inheritance/OverrideImpl2Example.java
@@ -0,0 +1,13 @@
+package org.ucdetector.example.inheritance;
+
+/**
+ *
+ */
+public class OverrideImpl2Example extends OverrideExample {
+	@Override
+	public void methodToOverride() {
+	}
+
+	public void unused2s() { // Marker YES: unused code
+	}
+}
