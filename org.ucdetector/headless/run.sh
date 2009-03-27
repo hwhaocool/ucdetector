78b3d9d84f239d3d685d4b646d28847d917a88dd
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-03-27 01:06:18 +0000

    headless build

--- org.ucdetector/headless/run.sh
+++ org.ucdetector/headless/run.sh
@@ -1,10 +1,11 @@
-#
-
 #export launcher=/opt/eclipse-3.3/plugins/org.eclipse.equinox.launcher_1.0.0.v20070606.jar
-export launcher=/opt/eclipse-3.4/plugins/org.eclipse.equinox.launcher_1.0.100.v20080509-1800.jar
 #export launcher=/opt/eclipse-3.4M5/plugins/org.eclipse.equinox.launcher_1.0.100.v20071211.jar
+export launcher=/opt/eclipse-3.4/plugins/org.eclipse.equinox.launcher_1.0.100.v20080509-1800.jar
 
-export workspace=F:/ws/ucd\/runtime-ucd_configuration
+export workspace=~/workspace/runtime-EclipseApplication
 
-java -jar $launcher -application org.eclipse.ant.core.antRunner -data $workspace -file headless.xml -consolelog -debug
+# Use ant runner
+#java -jar $launcher -application org.eclipse.ant.core.antRunner -data $workspace -file headless.xml -consolelog -debug -projects org.ucdetector.example,org.ucdetector
 
+# use ucd runner
+java -jar $launcher -application org.ucdetector.ucd -consolelog  -data $workspace -debug -projects org.ucdetector.example,org.ucdetector
