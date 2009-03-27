78b1699183148b87b43c34e618fe37cde48f3526
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-03-27 01:06:18 +0000

    headless build

--- trunk/org.ucdetector/headless/run.bat
+++ trunk/org.ucdetector/headless/run.bat
@@ -1,10 +1,10 @@
-rem set launcher=C:\eclipse\eclipse-3.3\plugins\org.eclipse.equinox.launcher_1.0.0.v20070606.jar
 set launcher=C:\eclipse\eclipse-3.4\plugins\org.eclipse.equinox.launcher_1.0.100.v20080509-1800.jar
+rem set launcher=C:\eclipse\eclipse-3.3\plugins\org.eclipse.equinox.launcher_1.0.0.v20070606.jar
 rem set launcher=C:\eclipse\eclipse-3.4.1\plugins\org.eclipse.equinox.launcher_1.0.101.R34x_v20080819.jar
 rem set launcher=C:\eclipse\eclipse-3.5M5\plugins\org.eclipse.equinox.launcher_1.0.200.v20090128-1500.jar
 
 set workspace=F:\ws\ucd\runtime-ucd_configuration
 
-rem java -jar %launcher% -application org.eclipse.ant.core.antRunner -data %workspace% -file headless.xml -consolelog -debug
-java -jar %launcher% -application org.ucdetector.ucd -consolelog  -data %workspace% -debug
+rem java -jar %launcher% -application org.eclipse.ant.core.antRunner -data %workspace% -file headless.xml -consolelog -debug -projects org.ucdetector.example,org.ucdetector
+java -jar %launcher% -application org.ucdetector.ucd -consolelog  -data %workspace% -debug -projects org.ucdetector.example,org.ucdetector
                                                           
