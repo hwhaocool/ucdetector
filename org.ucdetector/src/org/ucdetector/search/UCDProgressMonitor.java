5ab714890c9e4694c75d73b5765a02ecf6ec09b5
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-03-29 01:41:19 +0000

    logging

--- org.ucdetector/src/org/ucdetector/search/UCDProgressMonitor.java
+++ org.ucdetector/src/org/ucdetector/search/UCDProgressMonitor.java
@@ -29,15 +29,12 @@ public class UCDProgressMonitor implements IProgressMonitor {
 
   public void beginTask(String beginTaskName, int totalWork) {
     this.taskName = beginTaskName;
-    Log.logDebug("\n--------------------------------------------"); //$NON-NLS-1$
     Log.logInfo("Start task: " + beginTaskName); //$NON-NLS-1$
-    Log.logDebug(UCDetectorPlugin.getPreferencesAsString());
     delegate.beginTask(beginTaskName, totalWork);
   }
 
   public void done() {
     Log.logInfo("End task: " + taskName); //$NON-NLS-1$
-    Log.logDebug("--------------------------------------------\n"); //$NON-NLS-1$
     delegate.done();
   }
 
