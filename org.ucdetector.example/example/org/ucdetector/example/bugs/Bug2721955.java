925e74bfd681d435c7b98d5e89fdb25535562ebc
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-03-30 20:15:11 +0000

    example

--- org.ucdetector.example/example/org/ucdetector/example/bugs/Bug2721955.java
+++ org.ucdetector.example/example/org/ucdetector/example/bugs/Bug2721955.java
@@ -1,16 +1,34 @@
 package org.ucdetector.example.bugs;
+
 /**
  * i had two constants, both with UCD Marker QuickFix "Delete Code"
+ * 
  * <pre>
- * public static final String CON1 = "1";
- * public static final String CON2 = "2";
+ * public static final String CON1 = &quot;1&quot;;
+ * public static final String CON2 = &quot;2&quot;;
  * </pre>
- * I ran the quick fix on the first one and the both UDC Marker was deleted.
- * The code was only changed for the first quick fix.
+ * 
+ * I ran the quick fix on the first one and the both UDC Marker was deleted. The
+ * code was only changed for the first quick fix.
  */
 public class Bug2721955 {
-  public static final String UNUSED_1 = "1";
-  public static final String UNUSED_2 = "2";
-  public static final String UNUSED_3 = "2";
-  public static final String UNUSED_4 = "2";
+	// 1
+	public static final String UNUSED_1 = "1";
+	// 2
+	public static final String UNUSED_2 = "2";
+	// 3
+	public static final String UNUSED_3 = "2";
+	// 4
+	public static final String UNUSED_4 = "2";
+	// ------------------------------------------------------------------------
+	// PRIVATE
+	// ------------------------------------------------------------------------
+	// 2
+	private static final String UNUSED_PRIVATE_1 = "1";
+	// 2
+	private static final String UNUSED_PRIVATE_2 = "1";
+	// 3
+	private static final String UNUSED_PRIVATE_3 = "1";
+	// 4
+	private static final String UNUSED_PRIVATE_4 = "1";
 }
