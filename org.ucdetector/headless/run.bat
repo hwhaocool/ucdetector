2fb44f64846a75232335032e3e0b75568f476498
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-03-30 18:57:05 +0000

    +file header

--- org.ucdetector/headless/run.bat
+++ org.ucdetector/headless/run.bat
@@ -1,11 +1,20 @@
-set launcher=C:\eclipse\eclipse-3.4\plugins\org.eclipse.equinox.launcher_1.0.100.v20080509-1800.jar
-REM set launcher=C:\eclipse\eclipse-3.3\plugins\org.eclipse.equinox.launcher_1.0.0.v20070606.jar
-REM set launcher=C:\eclipse\eclipse-3.4.1\plugins\org.eclipse.equinox.launcher_1.0.101.R34x_v20080819.jar
-REM set launcher=C:\eclipse\eclipse-3.5M5\plugins\org.eclipse.equinox.launcher_1.0.200.v20090128-1500.jar
+REM ----------------------------------------------------------------------------
+REM  Copyright (c) 2009 Joerg Spieler All rights reserved. This program and the
+REM  accompanying materials are made available under the terms of the Eclipse
+REM  Public License v1.0 which accompanies this distribution, and is available at
+REM  http://www.eclipse.org/legal/epl-v10.html
+REM ----------------------------------------------------------------------------
 
-set workspace=F:\ws\ucd\runtime-ucd_configuration
+REM 'LAUNCHER' must point to launcher.jar which is located in ECLIPSE_HOME/plugins/
+set LAUNCHER=C:\eclipse\eclipse-3.4\plugins\org.eclipse.equinox.launcher_1.0.100.v20080509-1800.jar
+REM set LAUNCHER=C:\eclipse\eclipse-3.3\plugins\org.eclipse.equinox.launcher_1.0.0.v20070606.jar
+REM set LAUNCHER=C:\eclipse\eclipse-3.4.1\plugins\org.eclipse.equinox.launcher_1.0.101.R34x_v20080819.jar
+REM set LAUNCHER=C:\eclipse\eclipse-3.5M5\plugins\org.eclipse.equinox.launcher_1.0.200.v20090128-1500.jar
 
-REM To detect listet projects, use: '-projects org.example.project1,org.example.project2'
-REM To detect all projects, remove parameter: '-projects'
-java -jar %launcher% -application org.ucdetector.ucd -consolelog  -data %workspace% -debug -projects org.ucdetector.example,org.ucdetector
-                                                          
+REM WORKSPACE must point to your workspace directory (usuall parent directory of java projects)
+set WORKSPACE=F:\ws\ucd\runtime-ucd_configuration
+
+REM To run UCDetector for all projects: remove parameter: '-projects'
+REM To run UCDetector for listet projects use: '-projects org.example.project1,org.example.project2'
+java -jar %LAUNCHER% -application org.ucdetector.ucd -consolelog  -data %WORKSPACE% -debug -projects org.ucdetector.example,org.ucdetector
+
--- org.ucdetector/headless/run.sh
+++ org.ucdetector/headless/run.sh
@@ -1,7 +1,19 @@
-#export launcher=/opt/eclipse-3.3/plugins/org.eclipse.equinox.launcher_1.0.0.v20070606.jar
-#export launcher=/opt/eclipse-3.4M5/plugins/org.eclipse.equinox.launcher_1.0.100.v20071211.jar
-export launcher=/opt/eclipse-3.4/plugins/org.eclipse.equinox.launcher_1.0.100.v20080509-1800.jar
+# ------------------------------------------------------------------------------
+#  Copyright (c) 2009 Joerg Spieler All rights reserved. This program and the
+#  accompanying materials are made available under the terms of the Eclipse
+#  Public License v1.0 which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+# ------------------------------------------------------------------------------
 
-export workspace=~/workspace/runtime-EclipseApplication
+# 'LAUNCHER' must point to launcher.jar which is located in ECLIPSE_HOME/plugins/
+export LAUNCHER=/opt/eclipse-3.4/plugins/org.eclipse.equinox.launcher_1.0.100.v20080509-1800.jar
+#export LAUNCHER=/opt/eclipse-3.3/plugins/org.eclipse.equinox.launcher_1.0.0.v20070606.jar
+#export LAUNCHER=/opt/eclipse-3.4M5/plugins/org.eclipse.equinox.launcher_1.0.100.v20071211.jar
 
-java -jar $launcher -application org.ucdetector.ucd -consolelog  -data $workspace -debug -projects org.ucdetector.example,org.ucdetector
+# WORKSPACE must point to your workspace directory (usuall parent directory of java projects)
+export WORKSPACE=~/workspace/runtime-EclipseApplication
+
+# To run UCDetector for all projects: remove parameter: '-projects'
+# To run UCDetector for listet projects use: '-projects org.example.project1,org.example.project2'
+java -jar $LAUNCHER -application org.ucdetector.ucd -consolelog  -data $WORKSPACE -debug -projects org.ucdetector.example,org.ucdetector
+
