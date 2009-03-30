ca511f85b9c88a7ea27f7c25bc6845d939dbcfdc
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-03-30 16:08:47 +0000

    comments

--- trunk/org.ucdetector/headless/run.bat
+++ trunk/org.ucdetector/headless/run.bat
@@ -1,9 +1,11 @@
 set launcher=C:\eclipse\eclipse-3.4\plugins\org.eclipse.equinox.launcher_1.0.100.v20080509-1800.jar
-rem set launcher=C:\eclipse\eclipse-3.3\plugins\org.eclipse.equinox.launcher_1.0.0.v20070606.jar
-rem set launcher=C:\eclipse\eclipse-3.4.1\plugins\org.eclipse.equinox.launcher_1.0.101.R34x_v20080819.jar
-rem set launcher=C:\eclipse\eclipse-3.5M5\plugins\org.eclipse.equinox.launcher_1.0.200.v20090128-1500.jar
+REM set launcher=C:\eclipse\eclipse-3.3\plugins\org.eclipse.equinox.launcher_1.0.0.v20070606.jar
+REM set launcher=C:\eclipse\eclipse-3.4.1\plugins\org.eclipse.equinox.launcher_1.0.101.R34x_v20080819.jar
+REM set launcher=C:\eclipse\eclipse-3.5M5\plugins\org.eclipse.equinox.launcher_1.0.200.v20090128-1500.jar
 
 set workspace=F:\ws\ucd\runtime-ucd_configuration
 
+REM To detect listet projects, use: '-projects org.example.project1,org.example.project2'
+REM To detect all projects, remove parameter: '-projects'
 java -jar %launcher% -application org.ucdetector.ucd -consolelog  -data %workspace% -debug -projects org.ucdetector.example,org.ucdetector
                                                           
