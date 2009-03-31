f6b7300c6aa0053dd5bdf7f45efe466fa3233e0a
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-03-31 23:41:42 +0000

    Changed MixedExample for screenshots

--- org.ucdetector.example/src/test/org/ucdetector/example/impl/ReferenceHolderInTestSourceFolder.java
+++ org.ucdetector.example/src/test/org/ucdetector/example/impl/ReferenceHolderInTestSourceFolder.java
@@ -1,10 +1,14 @@
 package org.ucdetector.example.impl;
 
+import org.ucdetector.example.MixedExample;
 import org.ucdetector.example.ReferencedByTestsExample;
 
 public class ReferenceHolderInTestSourceFolder {
-  public static void main(String[] args) {
-    ReferencedByTestsExample example = new ReferencedByTestsExample();
-    example.referencedByTestSourceFolder();
-  }
+	public static void main(String[] args) {
+		ReferencedByTestsExample example = new ReferencedByTestsExample();
+		example.referencedByTestSourceFolder();
+
+		MixedExample.usedOnlyByTests();
+		MixedExample.usedOnlyByTests();
+	}
 }
