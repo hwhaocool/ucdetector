ed24c1ccdce8ca080b12e4882673afd5bc31db18
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-04-15 21:18:08 +0000

    better example

--- trunk/org.ucdetector.example/example/org/ucdetector/example/bugs/impl/Bug2539795Bar.java
+++ trunk/org.ucdetector.example/example/org/ucdetector/example/bugs/impl/Bug2539795Bar.java
@@ -2,5 +2,10 @@ package org.ucdetector.example.bugs.impl;
 
 //UC detector warning: Change visibility of "Bar" to default
 public class Bug2539795Bar { // Marker YES: use default
-	// Does nothing at all
+  // Does nothing at all
+
+  //  @Override
+  //  public String toString() {
+  //    return "bar string";
+  //  }
 }
