7f40bec965735ebf33266d8f1f26c80d0e60ad0a
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-04-15 21:18:08 +0000

    better example

--- org.ucdetector.example/example/org/ucdetector/example/bugs/Bug2539795Consumer.java
+++ org.ucdetector.example/example/org/ucdetector/example/bugs/Bug2539795Consumer.java
@@ -4,10 +4,10 @@ import org.ucdetector.example.bugs.impl.Bug2539795Foo;
 
 public class Bug2539795Consumer { // NO_UCD -- main method
 
-	public static void main(String[] args) {
-		// With the UCDetector suggestions, the following error is issued:
-		// The type Bar is not visible.
-		Bug2539795Foo foo = new Bug2539795Foo();
-		foo.getBar();
-	}
+  public static void main(String[] args) {
+    System.out.println("new Bug2539795Foo().getBar(): "
+        + new Bug2539795Foo().getBar());
+    new Bug2539795Foo().getBar();
+    new Bug2539795Foo().getBar().toString();
+  }
 }
