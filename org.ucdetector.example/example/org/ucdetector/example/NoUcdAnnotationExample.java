247487270bf5a942792c5061e8b0c71f33f18975
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-07-04 00:18:11 +0000

    +QuickFix 'Add @SuppressWarnings("UCD")'

--- org.ucdetector.example/example/org/ucdetector/example/NoUcdAnnotationExample.java
+++ org.ucdetector.example/example/org/ucdetector/example/NoUcdAnnotationExample.java
@@ -4,31 +4,76 @@ import org.ucdetector.example.classes.AnnotationExample;
 
 public class NoUcdAnnotationExample {
 
-	@SuppressWarnings( { "NO_UCD", "unused" })
-	/** javadoc 1 */
+	@java.lang.SuppressWarnings("ucd")
+	public int unused = 0;
+
+	@SuppressWarnings( { "ucd", "unused" })
+	/**
+	 * javadoc
+	 * 
+	 * 
+	 */
 	// comment 1
 	private final static String UNUSED_WITH_SINGLE_MEMBER_ANNOTATION = "1";
 
-	/** javadoc 1 */
-	@SuppressWarnings( { "NO_UCD", "unused" })
-	// comment 1
+	/** javadoc */
+	@SuppressWarnings( { "UCD", "test", "unused" })
+	/*
+	 * comment
+	 */
 	private final String unusedMember = "1";
+
+	// comment
+	@SuppressWarnings( { "UCD" })
+	/**
+	 * javadoc
+	 */
+	// comment
+	// comment
+	// comment
+	// comment
+	final String unusedMember2 = "1";
 
 	// comment 2
 	/** javadoc 1 */
-	@java.lang.SuppressWarnings("NO_UCD")
+	@java.lang.SuppressWarnings("ucd")
 	public void unused() {
 
 	}
 
 	@AnnotationExample(parameterExmaple = "1")
-	@SuppressWarnings("NO_UCD")
+	@SuppressWarnings("ucd")
 	public static final String UNUSED_WITH_NORMAL_ANNOTATION = "2";
 
-	// @SuppressWarnings("NO_UCD")
+	// @SuppressWarnings("ucd")
 	public static final String UNUSED_2 = "3"; // Marker YES: unused code
 
 	@SuppressWarnings("hello")
 	public static final String UNUSED_3 = "3"; // Marker YES: unused code
 
+	@SuppressWarnings("NO_UCD")
+	public static final String UNUSED_4 = "3"; // Marker YES: unused code
+
+	public class Unused {// Marker YES: unused code
+
+	}
+
+	@SuppressWarnings("ucd")
+	public class UnusedWithAnnotation {
+
+	}
+
+	public class UnusedWithOutAnnotation {// Marker YES: unused code
+		
+	}
+	
+	@SuppressWarnings("ucd")
+	public enum EnumExample {
+		UNUSED, //
+		UNUSED_2
+	}
+	
+	@java.lang.SuppressWarnings("ucd")
+	public @interface LocalAnnotationExample {
+	}
 }
