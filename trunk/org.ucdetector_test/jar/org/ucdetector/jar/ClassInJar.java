4b4a42c708bf5d10bbb0e526d8ffebd24d00f3a2
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-07-15 22:01:19 +0000

    docu

--- trunk/org.ucdetector_test/jar/org/ucdetector/jar/ClassInJar.java
+++ trunk/org.ucdetector_test/jar/org/ucdetector/jar/ClassInJar.java
@@ -3,18 +3,18 @@ package org.ucdetector.jar;
  * Example for org.ucdetector.example plugin
  * <pre>
  * method            =org.eclipse.jdt.internal.core.BinaryMethod
- * method            =org.eclipse.jdt.internal.core.BinaryField
+ * field             =org.eclipse.jdt.internal.core.BinaryField
  * ClassInJar        =org.eclipse.jdt.internal.core.BinaryType
  * ClassInJar.class  =org.eclipse.jdt.internal.core.ClassFile
  * org.ucdetector.jar=org.eclipse.jdt.internal.core.JarPackageFragment
  * example.jar       =org.eclipse.jdt.internal.core.JarPackageFragmentRoot
- * <pre>
  * Method                =org.eclipse.jdt.internal.core.SourceMethod
  * Field                 =org.eclipse.jdt.internal.core.SourceField
  * Java5Example          =org.eclipse.jdt.internal.core.SourceType
  * Java5Example.java     =org.eclipse.jdt.internal.core.CompilationUnit
  * org.ucdetector.example=org.eclipse.jdt.internal.core.PackageFragment
  * example               =org.eclipse.jdt.internal.core.PackageFragmentRoot
+ * </pre>
  */
 public class ClassInJar {
   public static final String CONSTANT = "CONSTANT";
