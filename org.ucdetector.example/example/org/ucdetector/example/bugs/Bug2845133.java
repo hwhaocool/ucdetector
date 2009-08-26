06361d5f6a6b75ea68e49345ddaa297dff97f67d
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-08-26 19:34:03 +0000

    +example bug 2845133

---
+++ org.ucdetector.example/example/org/ucdetector/example/bugs/Bug2845133.java
@@ -0,0 +1,12 @@
+package org.ucdetector.example.bugs;
+/**
+ * Bug 2845133: Default visibility QuickFix deletes javadoc
+ * <p>
+ * Details: Applying a default visibility quick fix to a class deletes also javadoc of the class
+ * <p>
+ * Submitted: Nobody/Anonymous ( nobody ) - 2009-08-26 21:22
+ * <p>
+ * See "https://sourceforge.net/tracker/?func=detail&aid=2845133&group_id=219599&atid=1046865"
+ */
+public class Bug2845133 { // Marker YES: use default
+}
