7186930299d36a1efb6a23212deae0d017bd967f
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-08-26 20:13:49 +0000

    added javadoc to understand example

--- org.ucdetector.example/example/org/ucdetector/example/bugs/Bug2539795Consumer.java
+++ org.ucdetector.example/example/org/ucdetector/example/bugs/Bug2539795Consumer.java
@@ -2,12 +2,20 @@ package org.ucdetector.example.bugs;
 
 import org.ucdetector.example.bugs.impl.Bug2539795Foo;
 
+/**
+ * See classes: Bug2539795Consumer, Bug2539795Bar, Bug2539795Foo<br>
+ * Changing Bug2539795Bar to private (suggested by UCDetector) causes a compile error in Bug2539795Consumer
+ * <p>
+ * https://sourceforge.net/tracker/?func=detail&aid=2539795&group_id=219599&atid=1046865
+ */
 public class Bug2539795Consumer { // NO_UCD -- main method
 
   public static void main(String[] args) {
-    System.out.println("new Bug2539795Foo().getBar(): "
-        + new Bug2539795Foo().getBar());
+
+    // NO compile error here, when changing visibility Bug2539795Bar to default
     new Bug2539795Foo().getBar();
+
+    // COMPILE ERROR HERE   , when changing visibility Bug2539795Bar to default
     new Bug2539795Foo().getBar().toString();
   }
 }
