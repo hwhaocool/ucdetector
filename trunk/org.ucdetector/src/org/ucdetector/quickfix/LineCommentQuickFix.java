00dd7fbd98bbf8cd2de0bfb08cf1b13486aa5c50
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2009-12-17 08:04:12 +0000

    no BlockComments

--- trunk/org.ucdetector/src/org/ucdetector/quickfix/LineCommentQuickFix.java
+++ trunk/org.ucdetector/src/org/ucdetector/quickfix/LineCommentQuickFix.java
@@ -19,10 +19,6 @@ import org.ucdetector.UCDetectorPlugin;
  * Fixes code by commenting all affected lines
  */
 class LineCommentQuickFix extends AbstractUCDQuickFix {
-  private static final String LINE_COMMENT = "// "; //$NON-NLS-1$
-  private static final String TODO_COMMENT //
-  = "TODO UCdetector: Remove unused code: "; //$NON-NLS-1$
-
   protected LineCommentQuickFix(IMarker marker) {
     super(marker);
   }
@@ -32,51 +28,16 @@ class LineCommentQuickFix extends AbstractUCDQuickFix {
     int offsetBody = nodeToChange.getStartPosition();
     int lengthBody = nodeToChange.getLength();
     int lineStart = doc.getLineOfOffset(offsetBody);
-    int startPositionLine = doc.getLineInformation(lineStart).getOffset();
     int lineEnd = doc.getLineOfOffset(offsetBody + lengthBody);
-    boolean useLineComments = containsBlockComment(lineStart, lineEnd);
-    String indent;
-    if (useLineComments) {
-      indent = createLineComments(lineStart, lineEnd);
-    }
-    else {
-      indent = createBlockComment(lineStart, lineEnd);
-    }
-    return startPositionLine + indent.length();
+    createLineComments(lineStart, lineEnd);
+    return doc.getLineInformation(lineStart).getOffset();
   }
 
   /**
-   * comment effected lines using <code>/* * /</code>
+   * comment effected lines using <code>// </code>
+   * See also ToggleCommentAction, TextViewer.shift(), ITextOperationTarget.PREFIX, ITextOperationTarget.STRIP_PREFIX<br>
    */
-  private String createBlockComment(int lineStart, int lineEnd)
-      throws BadLocationException {
-    String newLine = getLineDelimitter();
-    // end -----------------------------------------------------------------
-    IRegion region = doc.getLineInformation(lineEnd);
-    int offsetLine = region.getOffset();
-    int lengthLine = region.getLength();
-    String strLine = doc.get(offsetLine, lengthLine);
-    StringBuilder replaceEnd = new StringBuilder();
-    String indent = guessIndent(region);
-    replaceEnd.append(strLine).append(newLine);
-    replaceEnd.append(indent).append("*/"); //$NON-NLS-1$
-    doc.replace(offsetLine, lengthLine, replaceEnd.toString());
-    // start ---------------------------------------------------------------
-    region = doc.getLineInformation(lineStart);
-    offsetLine = region.getOffset();
-    lengthLine = region.getLength();
-    strLine = doc.get(offsetLine, lengthLine);
-    StringBuilder replaceStart = new StringBuilder();
-    replaceStart.append(indent).append("/* ").append(TODO_COMMENT); //$NON-NLS-1$
-    replaceStart.append(indent).append(newLine).append(strLine);
-    doc.replace(offsetLine, lengthLine, replaceStart.toString());
-    return indent;
-  }
-
-  /**
-   * comment effected lines using <code>//</code> 
-   */
-  private String createLineComments(int lineStart, int lineEnd)
+  private void createLineComments(int lineStart, int lineEnd)
       throws BadLocationException {
     String newLine = getLineDelimitter();
     // start at the end, because inserting new lines shifts following lines
@@ -86,36 +47,13 @@ class LineCommentQuickFix extends AbstractUCDQuickFix {
       int lengthLine = region.getLength();
       String strLine = doc.get(offsetLine, lengthLine);
       StringBuilder replace = new StringBuilder();
+      replace.append("// "); //$NON-NLS-1$
       if (lineNr == lineStart) {
-        replace/*.append(newLine)*/.append(LINE_COMMENT);
-        replace.append(TODO_COMMENT).append(newLine);
+        replace.append("TODO UCDetector: Remove unused code.").append(newLine); //$NON-NLS-1$
       }
-      replace.append(LINE_COMMENT).append(strLine);
+      replace.append(strLine);
       doc.replace(offsetLine, lengthLine, replace.toString());
     }
-    return ""; //$NON-NLS-1$
-  }
-
-  /**
-   * @return <code>true</code>, if "/*" or "*\" is found in one of the lines
-   */
-  private boolean containsBlockComment(int lineStart, int lineEnd)
-      throws BadLocationException {
-    for (int lineNr = lineStart; lineNr <= lineEnd; lineNr++) {
-      String line = getLine(lineNr);
-      if (line.indexOf("/*") != -1 || line.indexOf("*/") != -1) { //$NON-NLS-1$ //$NON-NLS-2$
-        return true;
-      }
-    }
-    return false;
-  }
-
-  /**
-   * @return line as String at lineNr
-   */
-  private String getLine(int lineNr) throws BadLocationException {
-    IRegion region = doc.getLineInformation(lineNr);
-    return doc.get(region.getOffset(), region.getLength());
   }
 
   public String getLabel() {
@@ -131,3 +69,31 @@ class LineCommentQuickFix extends AbstractUCDQuickFix {
     return null;
   }
 }
+//  /**
+//   * comment effected lines using <code>/* * /</code>
+//   */
+//  private String createBlockComment(int lineStart, int lineEnd)
+//      throws BadLocationException {
+//    String newLine = getLineDelimitter();
+//    // end -----------------------------------------------------------------
+//    IRegion region = doc.getLineInformation(lineEnd);
+//    int offsetLine = region.getOffset();
+//    int lengthLine = region.getLength();
+//    String strLine = doc.get(offsetLine, lengthLine);
+//    StringBuilder replaceEnd = new StringBuilder();
+//    String indent = guessIndent(region);
+//    replaceEnd.append(strLine).append(newLine);
+//    replaceEnd.append(indent).append("*/"); //$NON-NLS-1$
+//    doc.replace(offsetLine, lengthLine, replaceEnd.toString());
+//    // start ---------------------------------------------------------------
+//    region = doc.getLineInformation(lineStart);
+//    offsetLine = region.getOffset();
+//    lengthLine = region.getLength();
+//    strLine = doc.get(offsetLine, lengthLine);
+//    StringBuilder replaceStart = new StringBuilder();
+//    replaceStart.append(indent).append("/* ").append(TODO_COMMENT); //$NON-NLS-1$
+//    replaceStart.append(indent).append(newLine).append(strLine);
+//    doc.replace(offsetLine, lengthLine, replaceStart.toString());
+//    return indent;
+//  }
+
