c3994155c01200bce6acfeb6beacf066b980a936
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2010-01-05 00:44:37 +0000

    clean up

--- org.ucdetector.cycle/src/org/ucdetector/cycle/preferences/CyclePrefs.java
+++ org.ucdetector.cycle/src/org/ucdetector/cycle/preferences/CyclePrefs.java
@@ -10,14 +10,11 @@ package org.ucdetector.cycle.preferences;
 import org.ucdetector.cycle.CyclePlugin;
 
 public class CyclePrefs {
-  // CYCLE ---------------------------------------------------------------------
-  public static final String CYCLE_DEPTH //
-  = CyclePlugin.ID + ".cycleDepth"; //$NON-NLS-1$
-  public static final int CYCLE_DEPTH_MIN = 2; // // NO_UCD
-  public static final int CYCLE_DEPTH_DEFAULT = 4; //
-  public static final int CYCLE_DEPTH_MAX = 8; //
+  public static final String CYCLE_DEPTH = CyclePlugin.ID + ".cycleDepth"; //$NON-NLS-1$
+  public static final int CYCLE_DEPTH_MIN = 2;
+  public static final int CYCLE_DEPTH_DEFAULT = 4;
+  public static final int CYCLE_DEPTH_MAX = 8;
 
-  // CYCLE ---------------------------------------------------------------------
   /**
    * @return Maximum depth of searching for class cycles
    */
