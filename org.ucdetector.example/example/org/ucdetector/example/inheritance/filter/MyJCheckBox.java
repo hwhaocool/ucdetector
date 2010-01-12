aa7244a9c12076452bd9272629fd677d18c3481b
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2010-01-12 00:25:38 +0000

    Examples: [ 2929828 ] Added filter to exclude classes extending/implementing

---
+++ org.ucdetector.example/example/org/ucdetector/example/inheritance/filter/MyJCheckBox.java
@@ -0,0 +1,11 @@
+package org.ucdetector.example.inheritance.filter;
+
+import javax.swing.JCheckBox;
+
+/**
+ * Should be filtered by: javax.swing.AbstractButton or AbstractButton
+ */
+@SuppressWarnings("serial")
+public class MyJCheckBox extends JCheckBox {
+
+}
