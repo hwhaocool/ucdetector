cdfe4a3722a9dda6c178c20f9b6ff4b1e7137620
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2010-01-12 00:25:38 +0000

    Examples: [ 2929828 ] Added filter to exclude classes extending/implementing

---
+++ trunk/org.ucdetector.example/example/org/ucdetector/example/inheritance/filter/MyJTable.java
@@ -0,0 +1,11 @@
+package org.ucdetector.example.inheritance.filter;
+
+import javax.swing.JTable;
+
+/**
+ * Should be filtered by: javax.swing.event.CellEditorListener or CellEditorListener
+ */
+public class MyJTable extends JTable {
+  public static final long serialVersionUID = 1L;
+
+}
