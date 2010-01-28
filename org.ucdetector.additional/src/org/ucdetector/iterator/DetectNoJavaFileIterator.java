6d03a15ca50e99c053498c02366f702e1ca3fbf5
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2010-01-28 20:08:55 +0000

    added source examples to extend UCDetector

---
+++ org.ucdetector.additional/src/org/ucdetector/iterator/DetectNoJavaFileIterator.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2010 Joerg Spieler
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.ucdetector.iterator;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IJavaElement;
+
+/**
+ * Detect no java files
+ */
+@SuppressWarnings("nls")
+public class DetectNoJavaFileIterator extends AdditionalIterator {
+  private final StringBuilder report = new StringBuilder("No java files:\r\n");
+  private int noJavaFileCount = 0;
+
+  @Override
+  protected void handleResourceFile(IFile file) throws CoreException {
+    String name = file.getName();
+    if (!name.endsWith(".java") && !name.endsWith("class")) { //$NON-NLS-1$
+      report.append(file.getFullPath()).append("\r\n");//$NON-NLS-1$
+      noJavaFileCount++;
+    }
+  }
+
+  @Override
+  public void handleEndGlobal(IJavaElement[] objects) throws CoreException {
+    System.out.println(report);
+  }
+
+  @Override
+  protected boolean doResources() {
+    return true;
+  }
+
+  @Override
+  protected boolean doSelectedElement() {
+    return false;
+  }
+
+  /**
+   * do a simple "report"
+   */
+  @Override
+  public String getMessage() {
+    return "Found " + noJavaFileCount + " no java files";
+  }
+
+  @Override
+  public String getJobName() {
+    return "Detect no java file job";
+  }
+}
