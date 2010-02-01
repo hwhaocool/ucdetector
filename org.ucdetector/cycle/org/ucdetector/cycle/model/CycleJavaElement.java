c9fad25378b8e75e821549a65f1018d4731f02f7
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2010-02-01 23:36:42 +0000

    new source folder cycle

---
+++ org.ucdetector/cycle/org/ucdetector/cycle/model/CycleJavaElement.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2010 Joerg Spieler All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.ucdetector.cycle.model;
+
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.ui.JavaElementLabelProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.swt.graphics.Image;
+
+public abstract class CycleJavaElement extends CycleBaseElement /*implements IAdaptable*/{
+  private static final ILabelProvider LABEL_PROVIDER_DELEGAT = new JavaElementLabelProvider(
+      JavaElementLabelProvider.SHOW_SMALL_ICONS | //
+          JavaElementLabelProvider.SHOW_OVERLAY_ICONS | // 
+          JavaElementLabelProvider.SHOW_POST_QUALIFIED | //
+          JavaElementLabelProvider.SHOW_TYPE | //
+          JavaElementLabelProvider.SHOW_PARAMETERS//
+  );
+
+  //  JavaElementImageProvider ip = new JavaElementImageProvider();
+
+  /** @return the java element associated with the  CycleBaseElement, may be <code>null</code>*/
+  public abstract IJavaElement getJavaElement();
+
+  public Image getImage() {
+    return LABEL_PROVIDER_DELEGAT.getImage(getJavaElement());
+  }
+
+  public static String getDefaultText(IJavaElement javaElement) {
+    return LABEL_PROVIDER_DELEGAT.getText(javaElement);
+  }
+
+  //  @SuppressWarnings("unchecked")
+  //  public final Object getAdapter(Class adapter) {
+  //    if (adapter == IJavaElement.class) {
+  //      return getJavaElement();
+  //    }
+  //    else if (adapter == IWorkbenchAdapter.class) {
+  //      return new CycleWorkbenchAdapter(this);
+  //    }
+  //    else {
+  //      return null;
+  //    }
+  //    //    //    System.out.println("getAdapter: " + adapter.getName());
+  //    //    if (IContributorResourceAdapter.class == adapter) {
+  //    //      return CycleAdapterFactory.getInstance();
+  //    //    }
+  //    //    return CycleAdapterFactory.getInstance().getAdapter(getJavaElement(), adapter);
+  //  }
+}
---
+++ org.ucdetector/cycle/org/ucdetector/cycle/model/SearchResultRoot.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2010 Joerg Spieler
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.ucdetector.cycle.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.ISharedImages;
+import org.ucdetector.Messages;
+
+/**
+ * Contains all search results
+ * 
+ * <pre>
+ * <font color="red">SearchResultRoot</font> 
+ *   |- SearchResult
+ *     |- Cycle
+ *       |- CycleType
+ *         |- CycleMember
+ *           |- CycleRegion
+ * </pre>
+ */
+public class SearchResultRoot extends CycleBaseElement {
+  private static final SearchResultRoot INSTANCE = new SearchResultRoot();
+
+  private static final List<SearchResult> searchResults = new ArrayList<SearchResult>();
+
+  public static final SearchResultRoot getInstance() {
+    return INSTANCE;
+  }
+
+  public List<SearchResult> getChildren() {
+    return searchResults;
+  }
+
+  public Image getImage() {
+    return getImage(ISharedImages.IMG_TOOL_BACK);
+  }
+
+  /**
+   * @return always <code>null</code>
+   */
+  public ICycleBaseElement getParent() {
+    return null;
+  }
+
+  public String getText() {
+    return Messages.SearchResultRoot_Name;
+  }
+}
