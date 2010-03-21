17668441e180fcdbb72f68191c2f869cfb47347a
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2010-03-21 16:36:38 +0000

    +todos

--- trunk/org.ucdetector/src/org/ucdetector/search/VisibilityHandler.java
+++ trunk/org.ucdetector/src/org/ucdetector/search/VisibilityHandler.java
@@ -139,8 +139,15 @@ class VisibilityHandler {
       if (method.isMainMethod()) {
         return false;
       }
+      // Bug [ 2269486 ] Constants in Interfaces Can't be Private
       if (JavaElementUtil.isInterfaceMethod(method)) {
-        // fix bug [ 2269486 ] Constants in Interfaces Can't be Private
+        return false;
+      }
+      // TODO Bug [ 2968753] protected abstract method cannot be made private
+      // TODO: review all references from JavaElementUtil.isInterfaceMethod(method)
+      // TODO: review all marker creation of methods, check for isAbstract()
+      // TODO: review Bug2968753
+      if (Flags.isAbstract(method.getFlags())) {
         return false;
       }
     }
