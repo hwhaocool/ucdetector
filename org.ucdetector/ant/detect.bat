69edb4c3789efb90dd201aa1907c57fdc541bf57
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2010-09-25 00:07:43 +0000

    add run scripts again

---
+++ org.ucdetector/ant/detect.bat
@@ -0,0 +1,14 @@
+REM @echo off
+REM ----------------------------------------------------------------------------
+REM Copyright (c) 2010 Joerg Spieler All rights reserved. This program and the
+REM accompanying materials are made available under the terms of the Eclipse
+REM Public License v1.0 which accompanies this distribution, and is available at
+REM http://www.eclipse.org/legal/epl-v10.html
+REM ----------------------------------------------------------------------------
+
+set ECLIPSE_HOME=..\eclipse
+set WORKSPACE=..\workspace
+for /f "delims= tokens=1" %%c in ('dir /B /S /OD %ECLIPSE_HOME%\plugins\org.eclipse.equinox.launcher_*.jar') do set LAUNCHER=%%c
+
+java -Xmx1024m -jar %LAUNCHER% -clean -data %WORKSPACE% -application org.eclipse.ant.core.antRunner -buildfile build.xml run-ucdetector
+
---
+++ org.ucdetector/ant/detect.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#   ------------------------------------------------------------------------------
+#   Copyright (c) 2010 Joerg Spieler All rights reserved. This program and the
+#   accompanying materials are made available under the terms of the Eclipse
+#   Public License v1.0 which accompanies this distribution, and is available at
+#   http://www.eclipse.org/legal/epl-v10.html
+#   ------------------------------------------------------------------------------
+
+ECLIPSE_HOME=../eclipse
+WORKSPACE=../workspace
+LAUNCHER=$(find $ECLIPSE_HOME -name "org.eclipse.equinox.launcher_*.jar" | sort | tail -1);
+
+java -Xmx1024m -jar $LAUNCHER -clean -data $WORKSPACE -application org.eclipse.ant.core.antRunner -buildfile build.xml run-ucdetector
+
