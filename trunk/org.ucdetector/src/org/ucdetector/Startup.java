6f62c4ac92a6f5be0522c155b75e3b753a67c772
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2010-11-03 01:07:22 +0000

    GetUsedCodeVisitor

--- trunk/org.ucdetector/src/org/ucdetector/Startup.java
+++ trunk/org.ucdetector/src/org/ucdetector/Startup.java
@@ -8,7 +8,18 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IExecutionListener;
 import org.eclipse.core.commands.NotHandledException;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.FieldAccess;
+import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.IVariableBinding;
+import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
@@ -26,6 +37,7 @@ import org.ucdetector.util.JavaElementUtil;
 
 @SuppressWarnings("nls")
 public class Startup implements IStartup {
+
   public void earlyStartup() {
     Log.info("Startup.earlyStartup()");
     Display.getDefault().asyncExec(new Runnable() {
@@ -35,11 +47,11 @@ public class Startup implements IStartup {
           IPartService partService = activeWorkbenchWindow.getPartService();
           IPartListener partListener = new PartListener();
           partService.addPartListener(partListener);
-          //          Log.info("partListener added: " + partListener);
+          // Log.info("partListener added: " + partListener);
           ICommandService service = (ICommandService) activeWorkbenchWindow.getService(ICommandService.class);
           IExecutionListener commandListener = new ExecutionListener();
           service.addExecutionListener(commandListener);
-          //          Log.info("commandListener added: " + commandListener);
+          // Log.info("commandListener added: " + commandListener);
         }
         catch (Exception ex) {
           Log.error("Can't add listeners to active workbench window", ex);
@@ -48,47 +60,80 @@ public class Startup implements IStartup {
     });
   }
 
-  class ExecutionListener implements IExecutionListener {
+  // ==========================================================================
+  // PartListener
+  // ==========================================================================
+  private static final class PartListener implements IPartListener {
+
+    public void partActivated(IWorkbenchPart part) {
+      Log.info("PartListener.partActivated: " + part);
+      if (part instanceof IEditorPart) {
+        IEditorPart iEditorPart = (IEditorPart) part;
+        IType type = getTypeForPart(iEditorPart);
+        if (type != null) {
+          Log.info("      type is: " + type.getElementName());
+        }
+      }
+    }
+
+    public void partBroughtToTop(IWorkbenchPart part) {
+      // Log.info("PartListener.partBroughtToTop: " + part);
+    }
+
+    public void partClosed(IWorkbenchPart part) {
+      // Log.info("PartListener.partClosed: " + part);
+    }
+
+    public void partDeactivated(IWorkbenchPart part) {
+      // Log.info("PartListener.partDeactivated: " + part);
+    }
+
+    public void partOpened(IWorkbenchPart part) {
+      // Log.info("PartListener.partOpened: " + part);
+    }
+  }
+
+  // ==========================================================================
+  // ExecutionListener
+  // ==========================================================================
+  private static final class ExecutionListener implements IExecutionListener {
+    private final List<IType> typesBeforeSave = new ArrayList<IType>();
 
     public void preExecute(String commandId, ExecutionEvent event) {
+      typesBeforeSave.clear();
       Log.info("ExecutionListener.preExecute: " + commandId + ": " + event);
-      if (!isSave(commandId)) {
+      if (!isSaveCommand(commandId)) {
         return;
       }
-      List<IType> typesBeforeSave = new ArrayList<IType>();
       try {
         Log.info("PRE SAVE: " + event);
         WorkbenchPage page = UCDetectorPlugin.getActivePage();
         EditorManager editorManager = page.getEditorManager();
         IEditorPart[] dirtyEditors = editorManager.getDirtyEditors();
         for (IEditorPart iEditorPart : dirtyEditors) {
-          IEditorInput editorInput = iEditorPart.getEditorInput();
-          //            Log.info("    editorInput: " + editorInput);
-          if (editorInput instanceof IFileEditorInput) {
-            IFileEditorInput fileEditorInput = (IFileEditorInput) editorInput;
-            IFile file = fileEditorInput.getFile();
-            IType type = JavaElementUtil.getTypeFor(file);
-            if (type != null) {
-              typesBeforeSave.add(type);
-              Log.info("    PRE SAVE type: " + type.getElementName());
-            }
+          IType type = getTypeForPart(iEditorPart);
+          if (type != null) {
+            typesBeforeSave.add(type);
+            Log.info("    PRE SAVE type: " + type.getElementName());
           }
         }
       }
       catch (Exception ex) {
         Log.error("Can't get java types from save action", ex);
       }
-      Log.info("Types before save: %s", typesBeforeSave);
+      Log.info("Types before save: %s", JavaElementUtil.getElementNames(typesBeforeSave));
+      parseTypes(typesBeforeSave);
     }
 
     public void postExecuteSuccess(String commandId, Object returnValue) {
       Log.info("ExecutionListener.postExecuteSuccess: " + commandId + ": " + returnValue);
-      if (isSave(commandId)) {
-        Log.info("POST SAVE");
+      if (isSaveCommand(commandId)) {
+        Log.info("POST SAVE ");
+        parseTypes(typesBeforeSave);
       }
     }
 
-    private boolean isSave(String commandId) {
+    private boolean isSaveCommand(String commandId) {
       return IWorkbenchCommandConstants.FILE_SAVE.equals(commandId)
           || IWorkbenchCommandConstants.FILE_SAVE_ALL.equals(commandId);
     }
@@ -102,41 +147,62 @@ public class Startup implements IStartup {
     }
   }
 
-  class PartListener implements IPartListener {
+  private static void parseTypes(List<IType> typesBeforeSave) {
+    for (IType type : typesBeforeSave) {
+      ICompilationUnit compilationUnit = type.getCompilationUnit();
+      ASTParser parser = ASTParser.newParser(AST.JLS3);
+      parser.setSource(compilationUnit); // compilationUnit needed for resolve bindings!
+      parser.setKind(ASTParser.K_COMPILATION_UNIT);
+      parser.setResolveBindings(true);
+      ASTNode ast = parser.createAST(null);
+      GetUsedCodeVisitor visitor = new GetUsedCodeVisitor();
+      ast.accept(visitor);
+      Log.info("invokedFields : %s", JavaElementUtil.getElementNames(visitor.invokedFields));
+      Log.info("invokedMethods: %s", JavaElementUtil.getElementNames(visitor.invokedMethods));
+    }
+  }
 
-    public void partActivated(IWorkbenchPart part) {
-      Log.info("PartListener.partActivated: " + part);
+  private static IType getTypeForPart(IEditorPart iEditorPart) {
+    IEditorInput editorInput = iEditorPart.getEditorInput();
+    if (editorInput instanceof IFileEditorInput) {
+      IFileEditorInput fileEditorInput = (IFileEditorInput) editorInput;
+      IFile file = fileEditorInput.getFile();
+      return JavaElementUtil.getTypeFor(file);
+    }
+    return null;
+  }
+
+  // ==========================================================================
+  // ASTVisitor
+  // ==========================================================================
+  private static final class GetUsedCodeVisitor extends ASTVisitor {
+    List<IMethod> invokedMethods = new ArrayList<IMethod>();
+    List<IField> invokedFields = new ArrayList<IField>();
+
+    @Override
+    public boolean visit(MethodInvocation node) {
+      // Log.info("MethodInvocation: " + node.toString());
+      IMethodBinding methodBinding = node.resolveMethodBinding();
+      // Log.info("methodBinding: " + methodBinding);
+      IMethod methodFound = (IMethod) methodBinding.getJavaElement();
+      if (!methodFound.isBinary()) {
+        invokedMethods.add(methodFound);
+        Log.info("methodFound: " + methodFound.getElementName());
+      }
+      return true;
     }
 
-    public void partBroughtToTop(IWorkbenchPart part) {
-      Log.info("PartListener.partBroughtToTop: " + part);
-
-    }
-
-    public void partClosed(IWorkbenchPart part) {
-      Log.info("PartListener.partClosed: " + part);
-
-    }
-
-    public void partDeactivated(IWorkbenchPart part) {
-      Log.info("PartListener.partDeactivated: " + part);
-
-    }
-
-    public void partOpened(IWorkbenchPart part) {
-      Log.info("PartListener.partOpened: " + part);
+    @Override
+    public boolean visit(FieldAccess node) {
+      // Log.info("FieldAccess: " + node.toString());
+      IVariableBinding fieldBinding = node.resolveFieldBinding();
+      // Log.info("methodBinding: " + methodBinding);
+      IField field = (IField) fieldBinding.getJavaElement();
+      if (field.isBinary()) {
+        invokedFields.add(field);
+        Log.info("Field found: " + field.getElementName());
+      }
+      return true;
     }
   }
 }
-//        Log.info("   getParameters: " + event.getParameters());
-//        Log.info("   getApplicationContext: " + event.getApplicationContext());
-//          EvaluationContext ec = (EvaluationContext) event.getApplicationContext();
-
-// org.eclipse.ui.internal.SaveAllAction.run()
-//          if (ec == null) {
-//            return;
-//          }
-//          IEvaluationContext parentEc = ec.getParent();
-//          FileEditorInput activeEditorInput = (FileEditorInput) parentEc.getVariable("activeEditorInput");
-//          IFile file = activeEditorInput.getFile();
-//          Log.info("File: " + file);
