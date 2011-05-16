bfa036ace459e09a2bb16915ca9fecaa797be844
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2011-05-16 22:18:24 +0000

    +org.ucdetector.headless

---
+++ org.ucdetector/src/main/org/ucdetector/iterator/IteratorExtension.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2011 Joerg Spieler All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.ucdetector.iterator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ui.internal.WorkbenchPlugin;
+import org.ucdetector.UCDetectorPlugin;
+
+/**
+ * Provid access to the org.ucdetector.headless extension point
+ * <p>
+ * @author Joerg Spieler
+ * @since 2011-05-17
+ */
+public class IteratorExtension {
+  /** Simple identifier constant (value <code>"headless"</code>) for the UCDetector headless extension point. */
+  private static final String EXTENSION_POINT_ID = UCDetectorPlugin.ID + ".headless"; //$NON-NLS-1$
+  //
+  private static final String ATTRIBUTE_CLASS = "class";//$NON-NLS-1$
+  private static final String ATTRIBUTE_ID = "id";//$NON-NLS-1$
+  private static final String ATTRIBUTE_ORDINAL = "ordinal";//$NON-NLS-1$
+  private static boolean isInitialized = false;
+  private static ArrayList<IteratorExtension> iterators;
+
+  private final AbstractUCDetectorIterator iterator;
+  private final String id;
+  private final Integer ordinal;
+
+  private IteratorExtension(AbstractUCDetectorIterator iterator, String id, Integer ordinal) {
+    this.iterator = iterator;
+    this.id = id;
+    this.ordinal = ordinal;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  // STATIC -------------------------------------------------------------------
+  private static void loadExtensions() {
+    if (!isInitialized) {
+      isInitialized = true;
+      iterators = new ArrayList<IteratorExtension>();
+      IExtensionRegistry reg = Platform.getExtensionRegistry();
+      IConfigurationElement[] elements = reg.getConfigurationElementsFor(EXTENSION_POINT_ID);
+      for (IConfigurationElement element : elements) {
+        try {
+          String id = element.getAttribute(ATTRIBUTE_ID);
+          Integer ordinal = Integer.valueOf(element.getAttribute(ATTRIBUTE_ORDINAL));
+          AbstractUCDetectorIterator iterator = (AbstractUCDetectorIterator) WorkbenchPlugin.createExtension(//
+              element, ATTRIBUTE_CLASS);
+          iterators.add(new IteratorExtension(iterator, id, ordinal));
+        }
+        catch (Exception ex) {
+          UCDetectorPlugin.logToEclipseLog("Can't load ReportExtension", ex); //$NON-NLS-1$
+        }
+      }
+    }
+  }
+
+  public static ArrayList<IteratorExtension> getIterators() { // NO_UCD
+    loadExtensions();
+    Collections.sort(iterators, new IteratorExtensionSorter());
+    return iterators;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("IteratorExtension [id=%s, iterator=%s,]", id, iterator); //$NON-NLS-1$
+  }
+
+  private static final class IteratorExtensionSorter implements Comparator<IteratorExtension> {
+
+    public int compare(IteratorExtension o1, IteratorExtension o2) {
+      return o1.ordinal.compareTo(o2.ordinal);
+    }
+  }
+}
