58f001122834fa890944da931b33d4f7251a7d7e
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2011-06-23 01:18:19 +0000

    move headless options from build.xml to ucdetector.options

--- trunk/org.ucdetector/src/ant_tasks/org/ucdetector/ant/UCDTask.java
+++ trunk/org.ucdetector/src/ant_tasks/org/ucdetector/ant/UCDTask.java
@@ -6,10 +6,6 @@
  */
 package org.ucdetector.ant;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 import org.ucdetector.Log;
@@ -27,20 +23,13 @@ import org.ucdetector.UCDHeadless;
  * 
  */
 public class UCDTask extends Task {
-  private String sBuildType;
-  private String sOptionsFile;
-  private String sTargetPlatformFile;
-  private String sReport;
-  private final List<Iterate> iterateList = new ArrayList<Iterate>();
+  private String optionsFile;
 
   @Override
   public void execute() throws BuildException {
     Log.info("UCDetector ANT: 3 - Start java code of ant task '<ucdetector>' inside headless eclipse");
-    File targetPlatformFile = getFile(sTargetPlatformFile, "targetPlatformFile");
-    File optionsFile = getFile(sOptionsFile, "optionsFile");
-    List<String> resourcesToIterate = getResourcesToIterate();
     try {
-      new UCDHeadless(sBuildType, optionsFile, targetPlatformFile, sReport, resourcesToIterate).run();
+      new UCDHeadless(optionsFile).run();
     }
     catch (Exception e) {
       throw new BuildException(e);
@@ -48,66 +37,8 @@ public class UCDTask extends Task {
     Log.info("UCDetector ANT: 4 - End java code of ant task '<ucdetector>' inside headless eclipse");
   }
 
-  private List<String> getResourcesToIterate() {
-    List<String> result = new ArrayList<String>();
-    for (Iterate iterate : iterateList) {
-      Log.info("                        " + iterate);
-      result.add(iterate.getName());
-    }
-    return result;
-  }
-
-  private static File getFile(String fileName, String about) {
-    File result = null;
-    if (fileName != null) {
-      result = new File(fileName);
-      if (!result.exists()) {
-        throw new BuildException(about + " does not exist: " + result.getAbsolutePath());
-      }
-    }
-    return result;
-  }
-
-  // --------------------------------------------------------------------------
-  // ANT Attributes, Nested Elements
-  // http://ant.apache.org/manual/tutorial-writing-tasks.html
-  // --------------------------------------------------------------------------
-  public Iterate createIterate() {
-    Iterate i = new Iterate();
-    iterateList.add(i);
-    return i;
-  }
-
-  public class Iterate { // NO_UCD - used by ant
-    private String name;
-
-    public void setName(String name) {
-      this.name = name;
-    }
-
-    public String getName() {
-      return name;
-    }
-
-    @Override
-    public String toString() {
-      return name;
-    }
-  }
-
-  public void setBuildType(String buildType) {
-    this.sBuildType = buildType;
-  }
-
+  // ANT Attributes
   public void setOptionsFile(String optionsFile) {
-    this.sOptionsFile = optionsFile;
-  }
-
-  public void setTargetPlatformFile(String targetPlatformFile) {
-    this.sTargetPlatformFile = targetPlatformFile;
-  }
-
-  public void setReport(String report) {
-    this.sReport = report;
+    this.optionsFile = optionsFile;
   }
 }
