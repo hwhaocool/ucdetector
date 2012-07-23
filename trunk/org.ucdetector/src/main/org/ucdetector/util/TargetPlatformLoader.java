9efaaee31b88b8a6200ed35f932aa1dac180408c
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2012-07-23 21:13:06 +0000

    docu API change: ITargetDefinition

--- trunk/org.ucdetector/src/main/org/ucdetector/util/TargetPlatformLoader.java
+++ trunk/org.ucdetector/src/main/org/ucdetector/util/TargetPlatformLoader.java
@@ -18,6 +18,18 @@ import org.eclipse.pde.internal.core.target.provisional.LoadTargetDefinitionJob;
 import org.ucdetector.Log;
 import org.ucdetector.UCDHeadless;
 
+// == Since 4.2 ==
+//import org.eclipse.pde.core.target.ITargetDefinition;
+//import org.eclipse.pde.core.target.ITargetHandle;
+//import org.eclipse.pde.core.target.ITargetPlatformService;
+//import org.eclipse.pde.core.target.LoadTargetDefinitionJob;
+
+//== Before 4.2 ==
+//import org.eclipse.pde.internal.core.target.provisional.ITargetDefinition;
+//import org.eclipse.pde.internal.core.target.provisional.ITargetHandle;
+//import org.eclipse.pde.internal.core.target.provisional.ITargetPlatformService;
+//import org.eclipse.pde.internal.core.target.provisional.LoadTargetDefinitionJob;
+
 /**
  * Load target platform if a target platform file is declared in ucdetector options file.<p>
  * If plugin org.eclipse.pde.core is missing:
@@ -51,10 +63,14 @@ public class TargetPlatformLoader {
 
   private static void loadTargetPlatformImpl(IProgressMonitor monitor, File targetPlatformFile) throws CoreException {
     try {
-      ITargetPlatformService tps = (ITargetPlatformService) PDECore.getDefault().acquireService(
-          ITargetPlatformService.class.getName());
-      ITargetHandle targetHandle = tps.getTarget(targetPlatformFile.toURI());
-      ITargetDefinition targetDefinition = targetHandle.getTargetDefinition();
+      // See: org.eclipse.pde.internal.core.PluginModelManager.initializeTable()
+      ITargetPlatformService service = (ITargetPlatformService) PDECore.getDefault()//
+          .acquireService(ITargetPlatformService.class.getName());
+      if (service == null) {
+        return;
+      }
+      ITargetHandle handle = service.getTarget(targetPlatformFile.toURI());
+      ITargetDefinition targetDefinition = handle.getTargetDefinition();
       new LoadTargetDefinitionJob(targetDefinition).run(monitor);
     }
     catch (NoClassDefFoundError error) {
