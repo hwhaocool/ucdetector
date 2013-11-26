b7896dbbcf7e65b6fc83117f5488442bd6d7eb0a
spj <spj@7061ca7c-8a5b-4048-bfa6-2aa3552e9fd9>
2013-11-26 22:54:36 +0000

    Extracted headless code to new package headless

--- org.ucdetector/src/main/org/ucdetector/headless/HeadlessExtension.java
+++ org.ucdetector/src/main/org/ucdetector/headless/HeadlessExtension.java
@@ -4,7 +4,7 @@
  * Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.ucdetector.iterator;
+package org.ucdetector.headless;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,6 +17,7 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.ui.internal.WorkbenchPlugin;
 import org.ucdetector.Log;
 import org.ucdetector.UCDetectorPlugin;
+import org.ucdetector.iterator.AbstractUCDetectorIterator;
 
 /**
  * Provide access to the org.ucdetector.headless extension point
